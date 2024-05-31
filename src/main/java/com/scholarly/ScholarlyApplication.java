671fb87072a6a4917a4fe529fcf92a1bd50fc2b7
Nasser Mohamed <nassermohamedit@gmail.com>
2024-05-31 03:22:54 +0100

    initial commit

---
+++ src/main/java/com/scholarly/ScholarlyApplication.java
@@ -0,0 +1,13 @@
+package com.scholarly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarlyApplication.class, args);
+	}
+
+}
