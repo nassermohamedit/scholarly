cce5195a6bb8d3445878e947cb3cfd2c3f0653ff
Nasser Mohamed <nassermohamedit@gmail.com>
2024-06-02 20:24:56 +0100

    short -> Short for id field

--- src/main/java/com/scholarly/entity/Role.java
+++ src/main/java/com/scholarly/entity/Role.java
@@ -17,7 +17,7 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private short id;
+    private Short id;
 
     @Column(nullable = false, unique = true)
     private String name;
